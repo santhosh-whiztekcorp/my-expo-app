a94bb2a45f05cd7069982aa2cd5b25a457a362d1
Santhosh <santhosh@whiztekcorp.com>
2026-03-02 07:34:04 +0530

    feat: Introduce environment variables for configuration constants and add .env files.

--- src/constants/env/env.ts
+++ src/constants/env/env.ts
@@ -1,8 +1,8 @@
 export const ENV_CONSTANTS = {
   env: {
-    API_BASE_URL: 'https://jsonplaceholder.typicode.com',
-    ACCESS_TOKEN_KEY: 'auth.accessToken',
-    REFRESH_TOKEN_KEY: 'auth.refreshToken',
-    USER_INFO_KEY: 'auth.userInfo',
+    API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL ?? 'https://jsonplaceholder.typicode.com',
+    ACCESS_TOKEN_KEY: process.env.EXPO_PUBLIC_ACCESS_TOKEN_KEY ?? 'auth.accessToken',
+    REFRESH_TOKEN_KEY: process.env.EXPO_PUBLIC_REFRESH_TOKEN_KEY ?? 'auth.refreshToken',
+    USER_INFO_KEY: process.env.EXPO_PUBLIC_USER_INFO_KEY ?? 'auth.userInfo',
   },
 } as const;
